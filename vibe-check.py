30dc7ed3c25f5ea72427b96d59b6832eb4889965
Dasian <davidrespiritu99@gmail.com>
2023-02-01 21:47:34 -0500

    Added filesystem scanning

--- vibe-check.py
+++ vibe-check.py
@@ -5,11 +5,51 @@ from sort import *
 from item import *
 from benchmark import *
 import random
+import os
 
 def main():
+	# TODO generate items from spotify/plex
+	inp_path = '/home/dasian/Dasian/vibe-check/test'
+	split_inp = inp_path.split('/')
+	root_item = Item()
+	root_item.name = 'root'
+	root_item.type = 'root'
+	root_item.depth = 0
+	parent = None
+
+	# fill item tree from filesystem
+	# Items are folders and files
+	# folders have children while files don't
+	for root, dirs, files in os.walk(inp_path):
+		curr_path = root.split(os.sep)[len(split_inp):]
+		
+		# find parent
+		parent = root_item
+		for i in range(0, len(curr_path)):
+			parent = parent.get_child(curr_path[i])
+
+		# create children
+		children = dirs + files
+		depth = len(curr_path) + 1
+		# prompt to name this depth type? i.e. albums, shows
+		for c in children:
+			i = Item()
+			i.name = c
+			i.depth = depth
+			i.parent = parent
+			parent.children.append(i)
+
+	# get sorting info from user
+
+	# send warnings about running time
+
+	# run
+	
+
+def benchmark():
 	x = 1000
 	n = 600
-	k = 5		# smaller k, less comparisons for some reason??
+	k = 50		# smaller k, less comparisons for some reason??
 	rng = x*n	# range of values for testing
 
 	# test info
